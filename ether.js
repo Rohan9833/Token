7b59e3beaf3cbe959013d5b4265dfbc405fbd7eb
Rohan Pal <rohanpal9833@gmail.com>
2026-01-19 12:00:05 +0530

    added an react app

--- ether.js
+++ ether.js
@@ -1,4 +1,17 @@
 const {ethers} = require("ethers");
-require('dotenv').config({ path: __dirname + '/.env' });
+require('dotenv').config();
+const artifact = require("./artifacts/contracts/token.sol/token.json");
+const provider = new ethers.JsonRpcProvider(`https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`);
+const Contractaddress="0x6d361AC65264f299920691691D327d5Dc559c30f";
+const abi = artifact.abi;
 
-const provider = new ethers.provider.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_API_KEY}`);
+const queryBlockchain = async()=>{
+    const Token = new ethers.Contract(Contractaddress,abi,provider);
+
+    const contractname = await  Token.name();
+    console.log(contractname);
+
+
+}
+
+queryBlockchain();
